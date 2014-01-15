1d331064ebf33998b1445ceb252d8a110186b844
medvinsky.michael@gmail.com <medvinsky.michael@gmail.com@6278ae92-2c72-4582-1211-e073cfc0822c>
2014-01-15 07:20:10 +0000

    interior problem is now running, but still not working probably problem with Taylor, i.e. probably with the derivatives of coefficients.

--- branches/LaplacianDPM/+Solvers/InteriorLaplacianSolver.m
+++ branches/LaplacianDPM/+Solvers/InteriorLaplacianSolver.m
@@ -22,10 +22,9 @@ classdef InteriorLaplacianSolver < Solvers.SuperNonHomoSolver
                 
                 [X,Y] = GridK.mesh();
                 
-                error('TBD')
-                %  ScattK = struct('r',abs(X+1i.*Y),'r0',CoeffsAddParams.r0);%ScattererClsHandle(GridK,obj.ScattererAddParams);
-                %  WNPlr= Tools.Coeffs.CoeffsPolarR(ScattK,CoeffsAddParams.k0);
-                %  obj.k = sparse(WNPlr.k);
+                ScattK = struct('r',abs(X+1i.*Y));
+				obj.OpCoeffs=Tools.Coeffs.LaplaceCoeffsPolar(ScattK,CoeffsAddParams);
+				
             else
                 obj.OpCoeffs = CoeffsAddParams;
             end
@@ -33,17 +32,17 @@ classdef InteriorLaplacianSolver < Solvers.SuperNonHomoSolver
             BCinRhs=0;
             obj.Op =  Tools.DifferentialOps.LaplacianOp(Grid,obj.Coeffs,BCinRhs);
         end
-        
-        function u = P_Omega(obj,xi_gamma)
-            
-            rhs=zeros(obj.Grid.Nx,obj.Grid.Ny);
-            
-            rhs(obj.Scatterer.Mp)= obj.Lu(xi_gamma(:),obj.Scatterer.Mp);
-            GLW = obj.Gf(rhs(:));
-            
-            u = spalloc(obj.Grid.Nx,obj.Grid.Ny,numel(obj.Scatterer.Np));
-            u(obj.Scatterer.Np)=xi_gamma(obj.Scatterer.Np) - GLW(obj.Scatterer.Np).';                        
-        end
+		
+		function u = P_Omega(obj,xi_gamma)
+			
+			rhs=zeros(obj.Grid.Nx,obj.Grid.Ny);
+			
+			rhs(obj.Scatterer.Mp)= obj.Lu(xi_gamma(:),obj.Scatterer.Mp);
+			GLW = obj.Gf(rhs(:));
+			
+			u = spalloc(obj.Grid.Nx,obj.Grid.Ny,numel(obj.Scatterer.Np));
+			u(obj.Scatterer.Np)=xi_gamma(obj.Scatterer.Np) - GLW(obj.Scatterer.Np).' + obj.GF(obj.Scatterer.Np).';
+		end
     end
     
     methods(Access = protected)
@@ -65,31 +64,19 @@ classdef InteriorLaplacianSolver < Solvers.SuperNonHomoSolver
 		end
         
 		function rhs = Bf(obj,F)
-			%obj.Op.AdjustRhs
-% 			 F = F.';% probably no need
 			rhs = F(:);
 			% do nothing, required for compact scheme only 
 		end
 		
 		function res = BF(obj)
-			 
-			% 			 GridF = Tools.Grid.CartesianGrid( ...
-			% 				 obj.Grid.x1 - obj.Grid.dx/2 , ...
-			% 				 obj.Grid.xn + obj.Grid.dx/2 , ...
-			% 				 obj.Grid.Nx * 2 + 1         , ...
-			% 				 obj.Grid.y1 - obj.Grid.dy/2 , ...
-			% 				 obj.Grid.yn + obj.Grid.dy/2 , ...
-			% 				 obj.Grid.Ny * 2 + 1         ) ;
-			% 			 ScattererForSource = obj.ScattererClsHandle(GridF,obj.ScattererAddParams);
+			
 			ScattererForSource = obj.Scatterer;
 			
-			 HS = obj.SourceHandle(ScattererForSource,obj.CoeffsClsHandle,obj.CoeffsAddParams,obj.SourceParams);
-			 
-			 res = obj.Bf(HS.Source);
-% 			 res(obj.Scatterer.Outside())=0;
-			% res(obj.Scatterer.Inside())=0;
-			 
-		 end
+			HS = obj.SourceHandle(ScattererForSource,obj.CoeffsClsHandle,obj.CoeffsAddParams,obj.SourceParams);
+			
+			res = obj.Bf(HS.Source);
+			res(obj.Scatterer.Outside())=0;
+		end
 		
     end
         
