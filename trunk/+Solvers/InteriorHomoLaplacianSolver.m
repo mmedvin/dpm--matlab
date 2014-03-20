d4f360ea05d9d1e085033577815e2f4d809da203
medvinsky.michael@gmail.com <medvinsky.michael@gmail.com@6278ae92-2c72-4582-1211-e073cfc0822c>
2014-03-20 19:17:53 +0000

    added a missed file

---
+++ trunk/+Solvers/InteriorHomoLaplacianSolver.m
@@ -0,0 +1,64 @@
+classdef InteriorHomoLaplacianSolver < Solvers.SuperHomoSolver
+   
+    properties(Access = protected, AbortSet = true)
+        Op;
+        OpCoeffs;
+    end
+    
+    methods
+        function obj = InteriorHomoLaplacianSolver( ...
+                Basis,Grid,CoeffsClsHandle,CoeffsAddParams,ScattererClsHandle,ScattererAddParams)
+            obj = obj@Solvers.SuperHomoSolver( ...
+                Basis,Grid,CoeffsClsHandle,CoeffsAddParams,ScattererClsHandle,ScattererAddParams);
+            
+            if  numel(obj.Coeffs.a) + numel(obj.Coeffs.b) + numel(obj.Coeffs.sigma)>3
+                GridK = Tools.Grid.CartesianGrid( ...
+                    obj.Grid.x1 - obj.Grid.dx/2 , ...
+                    obj.Grid.xn + obj.Grid.dx/2 , ...
+                    2*obj.Grid.Nx + 1           , ...
+                    obj.Grid.y1 - obj.Grid.dy/2 , ...
+                    obj.Grid.yn + obj.Grid.dy/2 , ...
+                    2*obj.Grid.Ny + 1         ) ;
+                                
+                ScattK = struct('r',GridK.R);
+				obj.OpCoeffs=Tools.Coeffs.LaplaceCoeffsPolar(ScattK,CoeffsAddParams);
+				
+            else
+                obj.OpCoeffs = CoeffsAddParams;
+            end
+            
+            BCinRhs=0;
+            obj.Op =  Tools.DifferentialOps.LaplacianOp(Grid,obj.OpCoeffs,BCinRhs);
+        end
+		
+		function u = P_Omega(obj,xi_gamma)
+			
+			rhs=zeros(obj.Grid.Nx,obj.Grid.Ny);
+			
+			rhs(obj.Scatterer.Mp)= obj.Lu(xi_gamma(:),obj.Scatterer.Mp);
+			GLW = obj.Gf(rhs(:));
+			
+			u = spalloc(obj.Grid.Nx,obj.Grid.Ny,numel(obj.Scatterer.Np));
+			u(obj.Scatterer.Np)=xi_gamma(obj.Scatterer.Np) - GLW(obj.Scatterer.Np).';
+		end
+    end
+    
+    methods(Access = protected)
+      
+        function f = Lu(obj,u,msk)
+            if exist('msk','var');
+                f = obj.Op.A(msk,:)*u;
+            else
+                f = obj.Op.A*u;
+             end
+        end
+        
+        function u = Gf(obj,f)
+           u = obj.Op.A\f;           
+        end
+                
+        function Qj = Qcol(obj,GLW,~)
+            Qj = -GLW(obj.GridGamma,:);
+		end 
+	end
+end
