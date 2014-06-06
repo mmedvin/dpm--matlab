647f9b0c6a779a34ed31b6435313e31c8617be7e
medvinsky.michael@gmail.com <medvinsky.michael@gmail.com@6278ae92-2c72-4582-1211-e073cfc0822c>
2014-06-06 17:43:54 +0000

    minor bug in MatAmgWrapper.m fixed

--- RunIt/RunLaplacian351.m
+++ RunIt/RunLaplacian351.m
@@ -19,9 +19,7 @@ function RunLaplacian351
 
 	BType		= 'Fourier';
     
-    LinearSolverType = 0;
-    if LinearSolverType==0, CollectRhs = 1; else CollectRhs = 0;  end
-    
+     
     
     f   =@(phi) ExtExact(FocalDistance,Eta0,phi);
 	g   =@(phi) IntExact(FocalDistance,Eta0,phi);
@@ -35,10 +33,14 @@ function RunLaplacian351
 		Basis = Tools.Basis.FourierBasis.BasisHelper(f,g);%(@sin,@sin,1);%(f,dfdn);
 	end
 
+for	   LinearSolverType = 1
+    if LinearSolverType==0, CollectRhs = 1; else CollectRhs = 0;  end
+
+	
     fprintf('Problem 3.51, M=%d, LinearSolverType = %d, BIn=%d, BOut=%d\n', Basis.M, LinearSolverType, BIn, BOut);
     
     
-	for n=1:5 %run different grids
+	for n=1:4 %run different grids
 		tic
 		%build grid
 		p=3;%3;
@@ -215,7 +217,7 @@ function RunLaplacian351
     ErrTotPre = ErrTot;
     
 end
-
+end
 
 
 end
