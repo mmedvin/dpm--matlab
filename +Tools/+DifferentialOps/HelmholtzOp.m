d4ff9baa95d972ab61dfe5fe8ff980636af9362b
mmedvin <mmedvin@ncsu.edu>
2019-03-14 20:12:20 -0400

    minor

--- +Tools/+DifferentialOps/HelmholtzOp.m
+++ +Tools/+DifferentialOps/HelmholtzOp.m
@@ -1,2 +1,7 @@
 classdef HelmholtzOp<Tools.DifferentialOps.SuperHelmholtzOp
+    methods
+        function obj = HelmholtzOp(Arguments)
+            obj = obj@Tools.DifferentialOps.SuperHelmholtzOp(Arguments);
+        end
+    end
 end
